ccec6732a1163d144553510a2957c303ed4bbad3
mujaen <mujaen@nate.com>
2022-07-13 10:36:02 +0900

    Refactor: Web API 비동기 함수, 콜스택 예제 추가

--- js-example/index.js
+++ js-example/index.js
@@ -1,37 +1,31 @@
-// function A() {
-//   console.log(`A: ${B(200)}`)
-//   C()
-// }
-//
-// function B(num) {
-//   console.log('B')
-//   return num * 10000
-// }
-//
-// function C() {
-//   console.log('C')
-// }
-//
-// A()
+// 콜스택(순차실행) 예제
+const function2 = (x, y) => {
+  return console.log(x + y)
+}
+const function1 = (a, b) => {
+  function2(a, b)
+}
 
-// const count = () => {
-//   console.log('C')
-// };
-//
-// const nbFor = (max, load, block) => {
-//   let i = 0;
-//   const f = time => {
-//     let curr = load;
-//     while(curr-- && i < max) {
-//       block();
-//       i++;
-//     }
-//     if(i < max - 1) requestAnimationFrame(f);
-//   };
-//   requestAnimationFrame(f);
-// };
-//
-// nbFor(10000, 100, count);
+function1(1, 2)
 
-const count = (i) => {console.log('C')};
-for(let i = 0; i < 10000; i++) count(i);
+
+// Web Api 비동기 함수 예제
+const function2 = (maxCount = 1, callback) => {
+  let count = 0;
+  const interval = setInterval(() => {
+    if (count > maxCount) {
+      callback();
+      clearInterval(interval);
+    }
+    count++;
+    console.log(`함수 ${count}번 실행됨`);
+  }, 10);
+}
+
+const function1 = () => {
+  console.log('콜스택 입장');
+  function2(100, () => console.log('함수가 끝나면 나를 실행하시오'));
+  console.log('콜스택 퇴장');
+}
+
+function1();
